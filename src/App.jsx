886e32806eb02712ba20e2def92e6856dfd04e83
John Leonne <johnleonne@hotmail.com>
2023-03-12 15:33:06 -0400

    style: adiciona tema dinâmico no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import { React } from 'react';
+import React, { useMemo } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useMediaQuery } from '@mui/material';
 import TodoList from './pages/TodoList';
+import GlobalStyle from './components/css/GlobalStyle';
 
 function App() {
-  return <TodoList />;
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () => createTheme({
+      palette: {
+        mode: prefersDarkMode ? 'dark' : 'light',
+        background: {
+          default: prefersDarkMode ? '#424242' : '#e8e8e8',
+        },
+      },
+    }),
+    [prefersDarkMode],
+  );
+
+  return (
+    <ThemeProvider theme={ theme }>
+      <GlobalStyle theme={ theme } />
+      <TodoList />
+    </ThemeProvider>
+  );
 }
 
 export default App;
