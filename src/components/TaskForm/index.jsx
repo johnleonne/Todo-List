b734f6b37b13bc022ae0f4c54c69b4ac3f31656e
John Leonne <johnleonne@hotmail.com>
2023-03-12 15:36:00 -0400

    style: substitui as tags para as correspondentes do Material UI no componente TaskForm

--- src/components/TaskForm/index.jsx
+++ src/components/TaskForm/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button, Paper, TextField, Typography } from '@mui/material';
+import { Container } from '@mui/system';
 
 function TaskForm({ addTask }) {
   const [description, setDescription] = useState('');
@@ -16,21 +18,31 @@ function TaskForm({ addTask }) {
   };
 
   return (
-    <form onSubmit={ handleSubmit }>
+    <Paper sx={ { textAlign: 'center', padding: '24px', margin: '12px 0 ' } }>
+      <form onSubmit={ handleSubmit }>
+        <Typography variant="h4" style={ { marginBottom: '12px' } }>
+          Lista de Tarefas
+        </Typography>
+        <Container sx={ { display: 'flex', justifyContent: 'center' } }>
+          <TextField
+            id="outlined-basic"
+            label="Adicione uma tarefa"
+            variant="outlined"
+            value={ description }
+            onChange={ handleDescriptions }
+            sx={ { width: '100%' } }
+          />
 
-      <h1>Lista de Tarefas</h1>
-
-      <input
-        type="text"
-        id="AddTask"
-        placeholder="adicione uma tarefa"
-        value={ description }
-        onChange={ handleDescriptions }
-      />
-
-      <button type="submit">Adicionar</button>
-
-    </form>
+          <Button
+            type="submit"
+            variant="text"
+            sx={ { marginLeft: '12px', padding: '0 24px ' } }
+          >
+            Adicionar
+          </Button>
+        </Container>
+      </form>
+    </Paper>
   );
 }
 
